7e93efeb6852686737a1478676baa3085ce03d14
goldnest <94946762+goldnest@users.noreply.github.com>
2022-01-02 12:34:57 +0530

    Add files via upload
    2022

---
+++ revanumfl.java
@@ -0,0 +1,16 @@
+package learningJava;
+
+public class revanumfl {
+
+	public static void main(String[] args) {
+		int number = 654322, reverse = 0;  
+	 
+		for( ;number != 0; number=number/10)   
+		{  
+		int remainder = number % 10;  
+		reverse = reverse * 10 + remainder;  
+		}  
+		System.out.println("The reverse of the given number is: " + reverse);  
+		}  	
+
+	}
